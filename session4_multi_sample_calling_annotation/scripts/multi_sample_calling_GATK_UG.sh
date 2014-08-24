d6b14e170aa1a9ea14a3d2c24b418588b5009fe4
vplagnol <vincent.plagnol@gmail.com>
2014-08-24 22:50:10 +0100

    Further work on session 2, added samtools scripts and updated GATK UG ones

--- session4_multi_sample_calling_annotation/scripts/multi_sample_calling_GATK_UG.sh
+++ session4_multi_sample_calling_annotation/scripts/multi_sample_calling_GATK_UG.sh
@@ -1,9 +1,3 @@
-
-
-#### first create a list file that contains all the relevant BAM files
-### Note the .list suffix, which is important for GATK to know that this is a list of BAM files (and not a BAM file itself).
-find ../../data/BAM_files/ -name *bam > all_GBR_BAMs.list
-
 if [ ! -e tmp ]; then mkdir tmp; fi
 if [ ! -e results ]; then mkdir results; fi
 
@@ -11,13 +5,13 @@ if [ ! -e results ]; then mkdir results; fi
 GATK=/cluster/project8/vyp/vincent/Software/GenomeAnalysisTK-3.2-2/GenomeAnalysisTK.jar
 fasta=/cluster/project8/vyp/vincent/data/reference_genomes/fasta/human_g1k_v37.fasta
 
-#export PATH=$PATH:/cluster/project4/vyp/vincent/Software/samtools-0.1.18/bcftools/:/cluster/project4/vyp/vincent/Software/samtools-0.1.18
+#### first create a list file that contains all the relevant BAM files
+### Note the .list suffix, which is important for GATK to know that this is a list of BAM files (and not a BAM file itself).
+find ../data/BAM_files/ -name *bam > results/all_GBR_BAMs.list
 
-#samtools faidx data/chr21.fasta
-#java -jar /cluster/project4/vyp/vincent/Software/picard-tools-1.75/CreateSequenceDictionary.jar R=$fasta O=data/chr21.dict
 
 /share/apps/jdk1.7.0_25/jre/bin/java -Djava.io.tmpdir=tmp/ -Xmx4g -jar $GATK -T UnifiedGenotyper \
-    -I all_GBR_BAMs.list \
+    -I results/all_GBR_BAMs.list \
     -R $fasta -o results/GATK_msamplecalling.vcf --annotation QualByDepth\
     --annotation HaplotypeScore --annotation MappingQualityRankSumTest --annotation ReadPosRankSumTest --annotation FisherStrand\
     --annotation RMSMappingQuality --annotation Coverage --genotype_likelihoods_model BOTH \
--- session4_multi_sample_calling_annotation/scripts/multi_sample_calling_samtools.sh
+++ session4_multi_sample_calling_annotation/scripts/multi_sample_calling_samtools.sh
@@ -1,6 +1,21 @@
 export PATH=$PATH:/cluster/project4/vyp/vincent/Software/samtools-0.1.18/bcftools/:/cluster/project4/vyp/vincent/Software/samtools-0.1.18
 
-fasta=data/chr21.fasta
 
-samtools mpileup -uf $fasta -b data/GBR_bams.list | bcftools view -bvcg - > results/UBASH31A_samtools.bcf   
-bcftools view results/UBASH31A_samtools.bcf  > results/UBASH31A_samtools.vcf
+bcftools=/cluster/project8/vyp/vincent/Software/samtools-0.1.19/bcftools/bcftools
+samtools=/cluster/project8/vyp/vincent/Software/samtools-0.1.19/samtools
+
+fasta=../data/reference/chr21.fasta
+
+if [ ! -e tmp ]; then mkdir tmp; fi
+if [ ! -e results ]; then mkdir results; fi
+
+
+#### first create a list file that contains all the relevant BAM files
+### Note the .list suffix, which is important for GATK to know that this is a list of BAM files (and not a BAM file itself).
+find ../data/BAM_files/ -name *bam > results/all_GBR_BAMs.list
+
+
+
+
+$samtools mpileup -uf $fasta -b results/all_GBR_BAMs.list | $bcftools view -bvcg - > results/UBASH31A_samtools.bcf   
+$bcftools view results/UBASH31A_samtools.bcf  > results/UBASH31A_samtools.vcf
