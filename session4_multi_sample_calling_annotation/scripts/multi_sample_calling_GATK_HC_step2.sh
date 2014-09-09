f2c4ffce8ec2bb85b14e55aa1d84551b769ad36f
vplagnol <vincent.plagnol@gmail.com>
2014-09-09 23:47:11 +0100

    Polishing before the class

--- session4_multi_sample_calling_annotation/scripts/multi_sample_calling_GATK_HC_step2.sh
+++ session4_multi_sample_calling_annotation/scripts/multi_sample_calling_GATK_HC_step2.sh
@@ -2,13 +2,13 @@ if [ ! -e tmp ]; then mkdir tmp; fi
 if [ ! -e results ]; then mkdir results; fi
 
 ### Below the path to the GATK executable. It will need to be updated 
+#GATK=/usr/local/gatk/GenomeAnalysisTK.jar
 GATK=/cluster/project8/vyp/vincent/Software/GenomeAnalysisTK-3.2-2/GenomeAnalysisTK.jar
-fasta=/cluster/project8/vyp/vincent/data/reference_genomes/fasta/human_g1k_v37.fasta
+fasta=../data/human_g1k_v37.fasta
+
 
 #### first create a list file that contains all the relevant BAM files
 ### Note the .list suffix, which is important for GATK to know that this is a list of BAM files (and not a BAM file itself).
-
-
 find results/ -name *gvcf > results/gVCF_list.list
 
 java -Djava.io.tmpdir=tmp/ -Xmx4g -jar $GATK -T GenotypeGVCFs \
