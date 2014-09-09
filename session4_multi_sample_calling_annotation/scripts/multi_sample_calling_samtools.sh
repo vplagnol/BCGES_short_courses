dd2ecf538ede2659f8f91b400fafd5f9854a114e
vplagnol <vincent.plagnol@gmail.com>
2014-09-09 23:33:05 +0100

    Polishing before the class

--- session4_multi_sample_calling_annotation/scripts/multi_sample_calling_samtools.sh
+++ session4_multi_sample_calling_annotation/scripts/multi_sample_calling_samtools.sh
@@ -1,8 +1,5 @@
-export PATH=$PATH:/cluster/project4/vyp/vincent/Software/samtools-0.1.18/bcftools/:/cluster/project4/vyp/vincent/Software/samtools-0.1.18
+export PATH=/cluster/project8/vyp/vincent/Software/samtools-0.1.19/bcftools/:/cluster/project8/vyp/vincent/Software/samtools-0.1.19:$PATH
 
-
-bcftools=/cluster/project8/vyp/vincent/Software/samtools-0.1.19/bcftools/bcftools
-samtools=/cluster/project8/vyp/vincent/Software/samtools-0.1.19/samtools
 
 fasta=../data/reference/chr21.fasta
 
@@ -14,8 +11,5 @@ if [ ! -e results ]; then mkdir results; fi
 ### Note the .list suffix, which is important for GATK to know that this is a list of BAM files (and not a BAM file itself).
 find ../data/BAM_files/ -name *bam > results/all_GBR_BAMs.list
 
-
-
-
-$samtools mpileup -r 21:43823971-43867790 -Duf $fasta -b results/all_GBR_BAMs.list | $bcftools view -bvcg - > results/UBASH31A_samtools.bcf   
-$bcftools view results/UBASH31A_samtools.bcf  > results/UBASH31A_samtools.vcf
+samtools mpileup -r 21:43823971-43867790 -Duf $fasta -b results/all_GBR_BAMs.list | bcftools view -bvcg - > results/UBASH31A_samtools.bcf   
+bcftools view results/UBASH31A_samtools.bcf  > results/UBASH31A_samtools.vcf
