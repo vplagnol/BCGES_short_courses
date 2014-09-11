b295c01977de13bf154be8cba526c9fbed3c400d
vplagnol <vincent.plagnol@gmail.com>
2014-09-11 22:56:06 +0100

    Finalized session 7

--- session7_RNASeq/scripts/tophat_bowtie_scripts.sh
+++ session7_RNASeq/scripts/tophat_bowtie_scripts.sh
@@ -1,27 +1,19 @@
-tophatFolder=/cluster/project8/vyp/vincent/Software/tophat-2.0.8.Linux_x86_64
-bowtie2Folder=/cluster/project8/vyp/vincent/Software/bowtie2-2.1.0
-samtoolsFolder=/cluster/project8/vyp/vincent/Software/samtools-0.1.18
-cufflinksFolder=/cluster/project8/vyp/vincent/Software/cufflinks-2.0.2.Linux_x86_64
-
-
-export PATH=${bowtie2Folder}:${samtoolsFolder}:${tophatFolder}:${cufflinksFolder}:$PATH
-export PYTHONPATH=/cluster/project4/vyp/vincent/libraries/python/share/apps/python-2.7.1/lib/python2.7/site-packages:/cluster/project4/vyp/vincent/libraries/python/lib/python:$PYTHONPATH
-
+chmod u+x ./scripts/get_introns.pl
 
 #### build index
 bowtie2-build -f ../data/RNASeq/chr12_short.fa ../data/RNASeq/chr12_short
 
-mkdir results/tophat_output results/tophat_output_no_gtf results/cufflinks_output
+mkdir results/tophat_output_with_gtf results/tophat_output_no_gtf results/cufflinks_output
 
 f1=../data/RNASeq/reads_1.fq.gz
 f2=../data/RNASeq/reads_2.fq.gz
 
-tophat --no-coverage-search -o results/tophat_output -r 220 --library-type fr-unstranded --segment-length 30 -G ../data/RNASeq/chr12_short.gtf ../data/RNASeq/chr12_short ${f1} ${f2}
+tophat --no-coverage-search -o results/tophat_output_with_gtf -r 220 --library-type fr-unstranded --segment-length 30 -G ../data/RNASeq/chr12_short.gtf ../data/RNASeq/chr12_short ${f1} ${f2}
 cufflinks  -o results/cufflinks_output --GTF ../data/RNASeq/chr12_short.gtf results/tophat_output/accepted_hits.bam
 
 ####extract introns
-samtools index tophat_output/accepted_hits.bam
-samtools view  tophat_output/accepted_hits.bam 12:2055213-2113677 | awk '{if ($6 ~ /N/ ) {print;}}' | ./scripts/get_introns.pl | awk '{print $4"_"$5}'| sort | uniq -c > results/DCP1B.tab
+samtools index tophat_output_with_gtf/accepted_hits.bam
+samtools view  tophat_output_with_gtf/accepted_hits.bam 12:2055213-2113677 | awk '{if ($6 ~ /N/ ) {print;}}' | ./scripts/get_introns.pl | awk '{print $4"_"$5}'| sort | uniq -c > results/DCP1B.tab
 
 
 ##### now without the GTF
