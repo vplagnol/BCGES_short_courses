d281a29851c071f0ca3a3b76763dfa4dede13843
Vincent Plagnol <vincent.plagnol@inivata.com>
2015-08-31 23:53:33 +0100

    See CHANGELOGS

--- session4_multi_sample_calling_annotation/scripts/multi_sample_calling_GATK_HC_step2.sh
+++ session4_multi_sample_calling_annotation/scripts/multi_sample_calling_GATK_HC_step2.sh
@@ -2,8 +2,8 @@ if [ ! -e tmp ]; then mkdir tmp; fi
 if [ ! -e results ]; then mkdir results; fi
 
 ### Below the path to the GATK executable. It will need to be updated 
-#GATK=/usr/local/gatk/GenomeAnalysisTK.jar
-GATK=/cluster/project8/vyp/vincent/Software/GenomeAnalysisTK-3.2-2/GenomeAnalysisTK.jar
+GATK=/usr/local/gatk/GenomeAnalysisTK.jar
+##GATK=/cluster/project8/vyp/vincent/Software/GenomeAnalysisTK-3.2-2/GenomeAnalysisTK.jar
 fasta=human_g1k_v37.fasta
 
 
